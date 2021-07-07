7ff31ef0b1cd0ae8403c30374be7411bb747fb27
Almaz Kaliyev <almaz@kaliyev.dev>
2021-07-07 18:38:04 +0600

    Clean unusefull declarations

--- src/@types/custom.d.ts
+++ src/@types/custom.d.ts
@@ -1,20 +1,5 @@
 declare module 'redux-devtools-extension';
 
-declare module '*.avif' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.bmp' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.gif' {
-  const src: string;
-  export default src;
-}
-
 declare module '*.jpg' {
   const src: string;
   export default src;
@@ -29,12 +14,6 @@ declare module '*.png' {
   const src: string;
   export default src;
 }
-
-declare module '*.webp' {
-    const src: string;
-    export default src;
-}
-
 declare module '*.svg' {
   import * as React from 'react';
 
